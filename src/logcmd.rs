2b79914035d9327ff67bd22a77e308e915fc881b
Brian Anderson <andersrb@gmail.com>
2020-10-13 22:16:34 +0000

    .

---
+++ src/logcmd.rs
@@ -0,0 +1,29 @@
+use anyhow::Result;
+use serde::{Serialize, Deserialize};
+use std::io::{Read, Write};
+
+#[derive(Serialize, Deserialize)]
+pub enum LogCommand {
+    Write {
+        batch: u64,
+        key: Vec<u8>,
+        value: Vec<u8>,
+    },
+    Delete {
+        batch: u64,
+        key: Vec<u8>,
+    },
+    Commit {
+        batch: u64,
+    }
+}
+
+impl LogCommand {
+    pub fn write<W>(&self, writer: &mut W) -> Result<()> where W: Write {
+        panic!()
+    }
+
+    pub fn read<R>(reader: &mut R) -> Result<LogCommand> where R: Read {
+        panic!()
+    }
+}
