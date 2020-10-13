29aeb47fa880ba50e82b7bd4b608fb1ffb654c04
Brian Anderson <andersrb@gmail.com>
2020-10-13 04:44:17 +0000

    .

--- src/imp.rs
+++ src/imp.rs
@@ -32,25 +32,29 @@ mod inner {
     }
 
     impl DbInner {
-        pub fn new_batch(&self, tree: &str) -> Result<u64> { panic!() }
+        pub async fn open(config: DbConfig) -> Result<DbInner> { panic!() }
+    }
+
+    impl DbInner {
+        pub fn new_batch(&self) -> u64 { panic!() }
 
         pub fn write(&self, tree: &str, batch: u64, key: &[u8], value: &[u8]) -> Result<()> { panic!() }
 
         pub fn delete(&self, tree: &str, batch: u64, key: &[u8]) -> Result<()> { panic!() }
 
-        pub fn commit_batch(&self, tree: &str, batch: u64) -> Result<()> { panic!() }
+        pub fn commit_batch(&self, batch: u64) -> Result<()> { panic!() }
 
-        pub fn abort_batch(&self, tree: &str, batch: u64) -> Result<()> { panic!() }
+        pub fn abort_batch(&self, batch: u64) -> Result<()> { panic!() }
 
-        pub async fn sync(&self, tree: &str) -> Result<()> { panic!() }
+        pub async fn sync(&self) -> Result<()> { panic!() }
     }
 
     impl DbInner {
-        pub fn new_view(&self, tree: &str) -> Result<u64> { panic!() }
+        pub fn new_view(&self) -> u64 { panic!() }
 
         pub fn read(&self, tree: &str, view: u64, key: &[u8]) -> Result<Option<Vec<u8>>> { panic!() }
 
-        pub fn close_view(&self, tree: &str, view: u64) -> Result<()> { panic!() }
+        pub fn close_view(&self, view: u64) { panic!() }
     }
 }
 
@@ -61,35 +65,90 @@ pub type DbConfig = inner::DbConfig;
 #[derive(Clone)]
 pub struct Db(Arc<DbInner>);
 
-pub struct WriteBatch;
-pub struct ReadView;
-pub struct WriteTree;
-pub struct ReadTree;
+pub struct WriteBatch {
+    db: Arc<DbInner>,
+    batch: u64,
+    destructed: bool,
+}
+
+pub struct ReadView {
+    db: Arc<DbInner>,
+    view: u64,
+}
+
+pub struct WriteTree<'batch> {
+    batch: &'batch WriteBatch,
+    tree: String,
+}
+
+pub struct ReadTree<'view> {
+    view: &'view ReadView,
+    tree: String,
+}
 
 impl Db {
-    pub async fn open(config: DbConfig) -> Result<Db> { panic!() }
+    pub async fn open(config: DbConfig) -> Result<Db> {
+        DbInner::open(config).await.map(Arc::new).map(Db)
+    }
 
-    pub fn read_view(&self) -> ReadView { panic!() }
+    pub fn write_batch(&self) -> WriteBatch {
+        WriteBatch {
+            db: self.0.clone(),
+            batch: self.0.new_batch(),
+            destructed: false,
+        }
+    }
 
-    pub fn write_batch(&self) -> WriteBatch { panic!() }
+    pub fn read_view(&self) -> ReadView {
+        ReadView {
+            db: self.0.clone(),
+            view: self.0.new_view(),
+        }
+    }
+}
+
+impl Drop for WriteBatch {
+    fn drop(&mut self) {
+        if !self.destructed {
+            panic!("write batch not committed or aborted");
+        }
+    }
+}
+
+impl Drop for ReadView {
+    fn drop(&mut self) {
+        self.db.close_view(self.view);
+    }
 }
 
 impl WriteBatch {
-    pub fn tree(&self) -> Result<WriteTree> { panic!() }
+    pub fn tree<'batch>(&'batch self, tree: &str) -> WriteTree<'batch> {
+        WriteTree {
+            batch: self,
+            tree: tree.to_string(),
+        }
+    }
 
     pub async fn commit(self) -> Result<()> { panic!() }
+
+    pub fn abort(self) { panic!() }
 }
 
 impl ReadView {
-    pub fn tree(&self) -> Result<ReadTree> { panic!() }
+    pub fn tree<'view>(&'view self, tree: &str) -> ReadTree<'view> {
+        ReadTree {
+            view: self,
+            tree: tree.to_string(),
+        }
+    }
 }
 
-impl WriteTree {
+impl<'batch> WriteTree<'batch> {
     pub fn insert(&mut self, key: &[u8], value: &[u8]) -> Result<()> { panic!() }
 
     pub fn remove(&mut self, key: &[u8]) -> Result<()> { panic!() }
 }
 
-impl ReadTree {
+impl<'view> ReadTree<'view> {
     pub async fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>> { panic!() }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,29 +11,30 @@ pub struct Db(imp::Db);
 
 pub struct WriteBatch(imp::WriteBatch);
 pub struct ReadView(imp::ReadView);
-pub struct WriteTree(imp::WriteTree);
-pub struct ReadTree(imp::ReadTree);
+pub struct WriteTree<'batch>(imp::WriteTree<'batch>);
+pub struct ReadTree<'view>(imp::ReadTree<'view>);
 
 impl Db {
     pub async fn open(config: DbConfig) -> Result<Db> { imp::Db::open(config).await.map(Db) }
-    pub fn read_view(&self) -> ReadView { ReadView(self.0.read_view()) }
     pub fn write_batch(&self) -> WriteBatch { WriteBatch(self.0.write_batch()) }
+    pub fn read_view(&self) -> ReadView { ReadView(self.0.read_view()) }
 }
 
 impl WriteBatch {
-    pub fn tree(&self) -> Result<WriteTree> { self.0.tree().map(WriteTree) }
+    pub fn tree<'batch>(&'batch self, tree: &str) -> WriteTree<'batch> { WriteTree(self.0.tree(tree)) }
     pub async fn commit(self) -> Result<()> { self.0.commit().await }
+    pub fn abort(self) { self.0.abort() }
 }
 
 impl ReadView {
-    pub fn tree(&self) -> Result<ReadTree> { self.0.tree().map(ReadTree) }
+    pub fn tree<'view>(&'view self, tree: &str) -> ReadTree<'view> { ReadTree(self.0.tree(tree)) }
 }
 
-impl WriteTree {
+impl<'batch> WriteTree<'batch> {
     pub fn insert(&mut self, key: &[u8], value: &[u8]) -> Result<()> { self.0.insert(key, value) }
     pub fn remove(&mut self, key: &[u8]) -> Result<()> { self.0.remove(key) }
 }
 
-impl ReadTree {
+impl<'view> ReadTree<'view> {
     pub async fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>> { self.0.get(key).await }
 }
